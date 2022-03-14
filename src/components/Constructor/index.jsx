9d204d3750ede0033a80337fe10baa3eeeae063a
vagne0rlove9 <kirik-pos@mail.ru>
2022-03-14 19:05:28 +0300

    add styles and button

--- src/components/Constructor/index.jsx
+++ src/components/Constructor/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Editor } from "react-draft-wysiwyg";
+import React, { useState, useCallback } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { Button } from '@mui/material';
 
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './Constructor.css';
 
 const Constructor = () => {
+    const [editor, setEditor] = useState(EditorState.createEmpty());
+
+    const onEditorStateChange = useCallback((editorState) => {
+        setEditor(editorState);
+        console.log(editorState);
+    }, []);
+
+    const onTextChange = (event) => {
+        console.log(event);
+    };
+
     return (
-        <Editor />
+        <>
+            <Editor
+                editorState={editor}
+                onChange={onTextChange}
+                onEditorStateChange={onEditorStateChange}
+            />
+            <Button
+                variant="contained"
+                fullWidth
+                style={{ fontSize: '1rem', borderRadius: 0 }}
+            >
+                Отправить
+            </Button>
+        </>
     )
 }
 
